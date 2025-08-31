31d61be8187510916ede9338451b66729d9938bf
frankosparks-prog <mainafrank400@gmail.com>
2025-08-31 19:22:48 +0300

    Ready to begin !!

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code className='text-red-500'>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-blue-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
